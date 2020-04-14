567208835393958e1c02661525f28329a6f00dbe
Nguyen Huu Nhat <huunhat07@gmail.com>
2020-04-14 18:11:51 +0700

    init

---
+++ src/nguyenhuunhat_59131688_straterypattern/NguyenHuuNhat_59131688_StrateryPattern.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nguyenhuunhat_59131688_straterypattern;
+
+/**
+ *
+ * @author MSI
+ */
+public class NguyenHuuNhat_59131688_StrateryPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
